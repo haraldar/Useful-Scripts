bcf5698a0947b6c9770534b79a69efd629347043
haraldar <49565148+haraldar@users.noreply.github.com>
2022-01-04 18:31:48 +0100

    Root permission required and nmap wrapper function.
    The script now checks whether the script is running as root, because some of the nmap-commands require root privileges.

--- nmap/livehosts.sh
+++ nmap/livehosts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Author: Harald Asmus
-# Development period: Jan 1st, 2022 - Jan 3rd, 2022
+# Development period: Jan 1st, 2022 - Jan 4th, 2022
 # Reason: As a newbie to nmap trying to discover live responding hosts in my home
 #		is kind of inconsistent, so with this script I basically run all
 #		the different nmap ping flavors in one go and collect the
@@ -17,12 +17,16 @@
 # 	3 - split choose_ipv4 into get_ipv4s() and choose_ipv4s(), so that latter one may
 # 		return a string with echo instead of using a global.
 #	4 - clean the code and more documentation on code parts.
-#	5 - check if runs with admin privileges.
-#	6 - put 'sudo nmap -xX *-xX args | awk "filter_args"' into a seperate function.
+#	5 - check if script is run with admin privileges. --> DONE, Jan 4th 22
+#	6 - add wrapper function for the nmap-cmds in run_pings. - DONE, Jan 4th 22
+
+if [ "$EUID" -ne 0 ]
+	then echo "The script must be run as root."
+	exit
+fi
 
 end=""
 iprange=""
-readonly ip_filter='/Nmap s/ {print $NF}'
 
 function choose_ipv4(){
 	local ip_addrs=($(ip addr show | awk '/inet / {print $2}'))
@@ -35,21 +39,22 @@ function choose_ipv4(){
 	iprange=${ip_addrs[$ip_select]}
 }
 
+function nmap_wrap(){
+	local message=$2
+	echo "$message"
+	end+=$(sudo nmap $1 $iprange | awk '/Nmap s/ {print $NF}')
+	end+=" "
+}
+
 function run_pings(){
-	echo "Default Stealth ping ..."
-	end+=$(sudo nmap -sS $iprange | awk "$ip_filter")
-	end+=" "
-	echo "TCP ACK ping ..."
-	end+=$(sudo nmap -sA $iprange | awk "$ip_filter")
-	end+=" "
-	echo "TCP SYN ping ..."
-	end+=$(sudo nmap -sn -PS $iprange | awk "$ip_filter")
-	end+=" "
-	echo "UDP ping ..."
-	end+=$(sudo nmap -sn -PU $iprange | awk "$ip_filter")
-	end+=" "
-	echo "SCTP Init ping ..."
-	end+=$(sudo nmap -sY $iprange | awk "$ip_filter")
+	local flavors=("-sS" "-sA" "-sn -PS" "-sn -PU" "-sY")
+	local msgs=("Default stealth ping ..." "TCP ACK ping ..."
+			"TCP SYN ping ..." "UDP ping ..."
+			"SCTP Init ping ...")
+	for i in $(seq 0 $((${#flavors[@]}-1)))
+	do
+		nmap_wrap "${flavors[$i]}" "${msgs[$i]}"
+	done
 	end=($end)
 }
 
@@ -62,7 +67,7 @@ function distinctify_array(){
 function main(){
 	echo "Choose IPv4 address to scan."
 	choose_ipv4
-	echo "Running discovery pings now."
+	echo "Running discovery pings on $iprange now."
 	run_pings
 	echo "Finished."
 	local resultset=$(distinctify_array end[@])
