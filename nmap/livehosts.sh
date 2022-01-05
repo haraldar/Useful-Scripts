b8e1820a28d26de1bd595fdb51bc083e5ec15d1b
haraldar <49565148+haraldar@users.noreply.github.com>
2022-01-05 23:52:56 +0100

    Split choose_ipv4() & removed automatic hostname resolution.
    Split the choose_ipv4()-function into 3 logical parts causing less global variables to be needed (1 global variable remains [= end]). Also removed the automatic name resolution which makes the need for removing parentheses in the cleaning process of the resultset-array obsolete.

--- nmap/livehosts.sh
+++ nmap/livehosts.sh
@@ -12,13 +12,16 @@
 # 	3 - After selection immediately run pings and simply collect all responding IPs.
 #	4 - Finally clean the IPs of any errors and ouput the result.
 # TODO:
-#	1 - surpress the name resolution.
+#	1 - surpress the hostname resolution (makes the need to strip the
+#		parentheses obsolete). --> DONE, Jan 5th 22
 #	2 - use key value array for all filters.
 # 	3 - split choose_ipv4 into get_ipv4s() and choose_ipv4s(), so that latter one may
-# 		return a string with echo instead of using a global.
-#	4 - clean the code and more documentation on code parts.
+# 		return a string with echo instead of using a global. --> DONE, Jan 5th 22
+#	4 - clean the code and more documentation on code parts. -->  Jan 4th 22,
 #	5 - check if script is run with admin privileges. --> DONE, Jan 4th 22
 #	6 - add wrapper function for the nmap-cmds in run_pings. - DONE, Jan 4th 22
+#	7 - apparently there are compound switches for pings, need to built them in.
+#	8 - make the end-variable obsolete
 
 if [ "$EUID" -ne 0 ]
 	then echo "The script must be run as root."
@@ -26,7 +29,27 @@ if [ "$EUID" -ne 0 ]
 fi
 
 end=""
-iprange=""
+
+function get_ipv4s(){
+	local ip_addrs=($(ip addr show | awk '/inet / {print $2}'))
+	echo "${ip_addrs[@]}"
+}
+
+function display_options(){
+	local options="$1"
+	options=($options)
+	options_amount=$((${#options[@]}-1))
+	for i in $(seq 0 $options_amount)
+	do
+		echo "$i ${options[$i]}"
+	done
+}
+
+function select_option(){
+	read selection
+	local options=($1)
+	echo "${options[$selection]}"
+}
 
 function choose_ipv4(){
 	local ip_addrs=($(ip addr show | awk '/inet / {print $2}'))
@@ -40,9 +63,9 @@ function choose_ipv4(){
 }
 
 function nmap_wrap(){
-	local message=$2
+	local message=$3
 	echo "$message"
-	end+=$(sudo nmap $1 $iprange | awk '/Nmap s/ {print $NF}')
+	end+=$(sudo nmap -n $1 $2 | awk '/Nmap s/ {print $NF}')
 	end+=" "
 }
 
@@ -53,7 +76,7 @@ function run_pings(){
 			"SCTP Init ping ...")
 	for i in $(seq 0 $((${#flavors[@]}-1)))
 	do
-		nmap_wrap "${flavors[$i]}" "${msgs[$i]}"
+		nmap_wrap "${flavors[$i]}" "$1" "${msgs[$i]}"
 	done
 	end=($end)
 }
@@ -66,9 +89,12 @@ function distinctify_array(){
 
 function main(){
 	echo "Choose IPv4 address to scan."
-	choose_ipv4
-	echo "Running discovery pings on $iprange now."
-	run_pings
+	local ipv4s=$(get_ipv4s)
+	display_options "$ipv4s"
+	local selected_ipv4=$(select_option "$ipv4s")
+	echo "$selected_ipv4"
+	echo "Running discovery pings now."
+	run_pings "$selected_ipv4"
 	echo "Finished."
 	local resultset=$(distinctify_array end[@])
 	echo "Responding hosts:"
