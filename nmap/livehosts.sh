ba8bce016654f82cb4dee80e172540bc25988a6b
haraldar <49565148+haraldar@users.noreply.github.com>
2022-01-06 18:10:39 +0100

    Scan techniques are not hardcoded anymore.
    Scan techniques are now filtered from 'nmap --help' with the function nmap_techniques(). The parameter (default: 2) that is passed with the script will decide how many lines of scan techniques will be filtered (1-3 lines after the keyword "SCAN TECHNIQUES"). Ergo: It is not hardcoded anymore.

--- nmap/livehosts.sh
+++ nmap/livehosts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Author: Harald Asmus
-# Development period: Jan 1st, 2022 - Jan 4th, 2022
+# Development period: Jan 1st, 2022 - Jan 6th, 2022
 # Reason: As a newbie to nmap trying to discover live responding hosts in my home
 #		is kind of inconsistent, so with this script I basically run all
 #		the different nmap ping flavors in one go and collect the
@@ -13,19 +13,36 @@
 #	4 - Finally clean the IPs of any errors and ouput the result.
 # TODO:
 #	1 - surpress the hostname resolution (makes the need to strip the
-#		parentheses obsolete). --> DONE, Jan 5th 22
+#		parentheses obsolete).
+#			--> DONE, Jan 5th 22
 #	2 - use key value array for all filters.
+#			--> OBSOLETE, bc there's only 2 filters
 # 	3 - split choose_ipv4 into get_ipv4s() and choose_ipv4s(), so that latter one may
-# 		return a string with echo instead of using a global. --> DONE, Jan 5th 22
-#	4 - clean the code and more documentation on code parts. -->  Jan 4th 22,
-#	5 - check if script is run with admin privileges. --> DONE, Jan 4th 22
-#	6 - add wrapper function for the nmap-cmds in run_pings. - DONE, Jan 4th 22
+# 		return a string with echo instead of using a global.
+#			--> DONE, Jan 5th 22
+#	4 - clean the code and more documentation on code parts.
+#			-->  Jan 4th 22,
+#	5 - check if script is run with admin privileges.
+#			--> DONE, Jan 4th 22
+#	6 - add wrapper function for the nmap-cmds in run_pings.
+#			--> DONE, Jan 4th 22
 #	7 - apparently there are compound switches for pings, need to built them in.
+#			--> OBSOLETE, see pt.9
 #	8 - make the end-variable obsolete
+#			-->
+#	9 - get all scan techniques automatically from nmap -h so they dont have to be hardcoded
+#			--> DONE, Jan 6th 22
 
 if [ "$EUID" -ne 0 ]
 	then echo "The script must be run as root."
 	exit
+fi
+
+SCAN_LEVEL=$1
+if [[ $# == 0 ]]
+	then
+		echo "No SCAN_LEVEL parameter has been passed. Defaulting to 2."
+		SCAN_LEVEL=2
 fi
 
 end=""
@@ -63,20 +80,26 @@ function choose_ipv4(){
 }
 
 function nmap_wrap(){
-	local message=$3
-	echo "$message"
-	end+=$(sudo nmap -n $1 $2 | awk '/Nmap s/ {print $NF}')
+	echo "$1"
+	end+=$(sudo nmap -n -$1 $2 --host-timeout=5 | awk '/Nmap s/ {print $NF}')
 	end+=" "
 }
 
+function nmap_techniques(){
+	local techniques_section=$(nmap -h | grep "SCAN TECHNIQUES:" --after-context=$SCAN_LEVEL\
+		--line-buffered | awk '/ -s/ {print $1}')
+	techniques_section=$(tr '\n' ' ' <<< "$techniques_section" | tr -d ':' |\
+		tr -d '-' | tr '/' ' ' | xargs)
+	echo "$techniques_section"
+}
+
 function run_pings(){
-	local flavors=("-sS" "-sA" "-sn -PS" "-sn -PU" "-sY")
-	local msgs=("Default stealth ping ..." "TCP ACK ping ..."
-			"TCP SYN ping ..." "UDP ping ..."
-			"SCTP Init ping ...")
+	local flavors=$(nmap_techniques)
+	flavors=($flavors)
+#	local responding=""
 	for i in $(seq 0 $((${#flavors[@]}-1)))
 	do
-		nmap_wrap "${flavors[$i]}" "$1" "${msgs[$i]}"
+		nmap_wrap "${flavors[$i]}" "$1"
 	done
 	end=($end)
 }
@@ -88,6 +111,7 @@ function distinctify_array(){
 }
 
 function main(){
+	nmap_techniques
 	echo "Choose IPv4 address to scan."
 	local ipv4s=$(get_ipv4s)
 	display_options "$ipv4s"
